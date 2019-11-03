f9b3f74fd888a5a21afe4701af5b21adf42c7eba
Brian K Izaki <bkizaki@outlook.com>
2019-11-03 00:46:11 -0300

    alteração de texto da página inicial

--- script_banco/DDL-Musi.sql
+++ script_banco/DDL-Musi.sql
@@ -54,3 +54,61 @@ CREATE TABLE aula(
 	CONSTRAINT fk_matricula_aula FOREIGN KEY (id_matricula) REFERENCES matricula(id_matricula),
 	CONSTRAINT fk_professor_aula FOREIGN KEY (id_prof) REFERENCES professores(id_prof)
 );
+
+--/my SQL---------------------------------------------------------------------=-
+CREATE TABLE login(
+	id_login integer auto_increment,
+	email varchar(50) not null,
+	senha varchar(30) not null,
+	nv_acesso varchar(50) not null,
+	CONSTRAINT pk_login PRIMARY KEY (id_login),
+	CONSTRAINT uni_login UNIQUE (email)
+);
+
+CREATE TABLE alunos (
+	id_aluno integer auto_increment ,
+	nome varchar(50) not null,
+	dt_nasc date not null,
+	cpf varchar(15) not null,
+	cel integer,
+	CONSTRAINT pk_aluno PRIMARY KEY (id_aluno),
+	CONSTRAINT uni_cpf_aluno UNIQUE (cpf)
+);
+
+CREATE TABLE materia(
+	id_materia integer AUTO_INCREMENT,
+	nome varchar(50),
+	tipo varchar(50),
+	CONSTRAINT pk_materia PRIMARY KEY (id_materia)
+);
+
+CREATE TABLE matricula(
+	id_matricula integer auto_increment,
+	id_materia integer,
+	id_aluno integer,
+	dt_hr_matricula date,
+	CONSTRAINT pk_matricula PRIMARY KEY (id_matricula),
+	CONSTRAINT fk_materia_matricula FOREIGN KEY (id_materia) REFERENCES materia(id_materia),
+	CONSTRAINT fk_aluno_matricula FOREIGN KEY (id_aluno) REFERENCES alunos(id_aluno)
+);
+
+CREATE TABLE professores(
+	id_prof integer auto_increment,
+	nome varchar(100) not null, 
+	dt_nasc date not null,
+	cpf varchar(15) not null,
+	curso varchar(50),
+	CONSTRAINT pk_professor PRIMARY KEY (id_prof),
+	CONSTRAINT uni_cpf_professor UNIQUE (cpf)
+);
+
+CREATE TABLE aula(
+	id_aula integer auto_increment,
+	id_matricula integer,
+	id_prof integer,
+	dia_semana varchar(10),
+	dt_hr date,
+	CONSTRAINT pk_aula PRIMARY KEY (id_aula),
+	CONSTRAINT fk_matricula_aula FOREIGN KEY (id_matricula) REFERENCES matricula(id_matricula),
+	CONSTRAINT fk_professor_aula FOREIGN KEY (id_prof) REFERENCES professores(id_prof)
+);
